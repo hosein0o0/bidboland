31467b9014c220bef334c4e3f83b188e31dfb71d
hosein0o0 <hosein.derakhshain76@gmail.com>
2025-03-14 00:54:35 -0700

    merge

--- src/staticData.js
+++ src/staticData.js
@@ -16,16 +16,16 @@ switch (ProjectName) {
     status = 'سامانه یکپارچه تصمیم ساز مدیریت پروژه دارا'
     break
   case 'BIDBOLAND':
-    FrontIp = 'http://192.168.178.25:8080'
+    // FrontIp = 'http://192.168.178.25:8080'
     // FrontIp = 'http://192.168.30.81:8083/http://192.168.178.15'
-    // FrontIp = 'http://185.190.39.82:8050'
+    FrontIp = 'http://185.190.39.82:8050'
     // FrontIp = '/refrence'
     domainIp = FrontIp + ''
     // logoUrl = 'http://185.105.239.21'
     Title = 'DARA IDMS'
     Name = 'بیدبلند خلیج فارس'
     status = 'سامانه مدیریت اسناد الکترونیک دارا'
-    link_file = `http://192.168.178.25`
+    link_file = `http://185.190.39.82:85`
     break
   case 'Pars':
     FrontIp = 'http://185.105.239.21:8083/185.105.239.21:8080'
@@ -39,11 +39,8 @@ switch (ProjectName) {
     break
 }
 module.exports = {
-  domainIp,
-  FrontIp,
-  Title,
-  Name,
+  domainIp, FrontIp,
+  Title, Name,
   ProjectName,
-  status,
-  link_file
+  status, link_file
 }
--- src/tsrNew/report/content.js
+++ src/tsrNew/report/content.js
@@ -4,25 +4,22 @@ import { LastStatusForm } from './lastStatusForm'
 import { get } from '../../api'
 import { LastStatusOffice } from './lastStatusOffice'
 import { LastStatusDiscipline } from './lastStatusDiscipline'
-import { LastStatusImprove } from './lastStatusImprove'
-export function ContentReport () {
+export function ContentReport() {
   const [formList, setFormList] = useState([])
   const [officeList, setOfficeList] = useState([])
   const [disciplineList, setDisciplineList] = useState([])
   const [dataInfo, setDataInfo] = useState()
-  const [improvementList, setImprovementList] = useState([])
   useEffect(() => {
     fuetchData()
   }, [])
   const fuetchData = async () => {
     const generalInfo = await get('tsrMg/get')
+
     setFormList(generalInfo?.form_list ?? [])
 
     setOfficeList(generalInfo?.office_list ?? [])
 
     setDisciplineList(generalInfo?.discipline_list ?? [])
-
-    setImprovementList(generalInfo?.improvement_list ?? [])
 
     setDataInfo(() => generalInfo)
   }
@@ -35,8 +32,6 @@ export function ContentReport () {
       <LastStatusOffice list={officeList} />
       <hr />
       <LastStatusDiscipline list={disciplineList} />
-      <hr />
-      <LastStatusImprove list={improvementList} />
     </>
   )
 }
